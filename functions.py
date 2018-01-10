80b3c69f160ae623c14d2acae288247fe1d1df58
Tim Colbert <admin@tcolbert.net>
2018-01-10 16:01:09 +1000

    Update functions file

--- functions.py
+++ functions.py
@@ -10,5 +10,5 @@ def json_file_open(fname):
         os.path.isfile(fname)
         return json.loads(open(fname).read())
     except Exception, e:
-        print 'File not found in ' + str(os.getcwd()) + ': ' + str(e)
+        print('File not found in ' + str(os.getcwd()) + ': ' + str(e))
         sys.exit(1)
